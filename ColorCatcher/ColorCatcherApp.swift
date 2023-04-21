57f348d20e46338de47b71a67e64f27256cc3056
Rajesh sharma <rajesh.rajesh@publicissapient.com>
2023-04-21 21:10:31 +0530

    Initial Commit

---
+++ ColorCatcher/ColorCatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorCatcherApp.swift
+//  ColorCatcher
+//
+//  Created by Rajesh Rajesh on 21/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ColorCatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
